7285087488fc83840872ca6ab2d27f1b3bdcc981
xdee201101 <datnxph36362@fpt.edu.vn>
2024-11-04 17:07:11 +0700

    Viết code DTO

--- app/src/main/java/com/xdee/lab1_datnxph36362/DTO/CatDTO.java
+++ app/src/main/java/com/xdee/lab1_datnxph36362/DTO/CatDTO.java
@@ -1,4 +1,27 @@
 package com.xdee.lab1_datnxph36362.DTO;
 
 public class CatDTO {
+    int id;
+    String name;
+
+    public CatDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- app/src/main/java/com/xdee/lab1_datnxph36362/DTO/ProductDTO.java
+++ app/src/main/java/com/xdee/lab1_datnxph36362/DTO/ProductDTO.java
@@ -1,4 +1,45 @@
 package com.xdee.lab1_datnxph36362.DTO;
 
 public class ProductDTO {
+    int id, price, id_cat;
+    String name;
+
+    public ProductDTO(int id, int price, int id_cat, String name) {
+        this.id = id;
+        this.price = price;
+        this.id_cat = id_cat;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId_cat() {
+        return id_cat;
+    }
+
+    public void setId_cat(int id_cat) {
+        this.id_cat = id_cat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
